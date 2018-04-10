622205fa7585279bf93b1a8e555ac1698586d61f
Jamila90 <jamila.shakurova@gmail.com>
2018-04-10 23:34:43 +0400

    Выполняет пункты 1-3

--- js/pictures.js
+++ js/pictures.js
@@ -11,7 +11,7 @@ var COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var DESCRIPTION = [
+var DESCRIPTIONS = [
   'Тестим новую камеру!',
   'Затусили с друзьями на море',
   'Как же круто тут кормят',
@@ -19,8 +19,6 @@ var DESCRIPTION = [
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'
 ];
-
-var photoList = [];
 
 function getRandomNumber(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
@@ -36,21 +34,41 @@ function shuffle(arr) {
   return arr;
 }
 
-function createPhoto(amount) {
+var ShowAllPhoto = generatePhotoCards(25);
+
+function generatePhotoCards(amount) {
+
+  var photoList = [];
 
   for (var i = 0; i < amount; i++) {
-    var description = shuffle(DESCRIPTION).slice(0, getRandomNumber(1, DESCRIPTION.length));
-    var comments = shuffle(COMMENTS).getRandomNumber(1, 2);
+    var description = shuffle(DESCRIPTIONS);
+    var comments = shuffle(COMMENTS);
 
     var photoAd = {
-      url: 'photos/' + getRandomNumber(1, 25),
-      likes: getRandomNumber(LIKES),
-      comments: comments,
-      description: description
+      url: 'photos/' + (i + 1) + '.jpg',
+      likes: getRandomNumber(LIKES[0], LIKES[1]),
+      comments: getRandomNumber(0, 1) ? comments.slice(0, 1) : comments.slice(0, 2),
+      description: description[getRandomNumber(0, description.length)]
     };
     photoList.push(photoAd);
   }
   return photoList;
 }
 
-photoList = createPhoto(25);
+function renderPictures(arr) {
+  var picturesTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+  var pictures = document.querySelector('.pictures');
+
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < arr.length; i++) {
+    var picturesElement = picturesTemplate.cloneNode(true);
+    picturesElement.querySelector('img').src = arr[i].url;
+    picturesElement.querySelector('.picture__stat--likes').textContent = arr[i].likes;
+    picturesElement.querySelector('.picture__stat--comments').textContent = arr[i].comments.length;
+    fragment.appendChild(picturesElement);
+  }
+  pictures.appendChild(fragment);
+}
+
+renderPictures(ShowAllPhoto);
